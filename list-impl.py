71a2c593aa2d710b9b56aaf3c0304d3e593226db
Matthew Walinga <matthew.walinga@gmail.com>
2017-07-14 23:47:48 -0400

    Added random remove function, and some tests

--- list-impl.py
+++ list-impl.py
@@ -1,11 +1,12 @@
 #
 ## Useful List Implementations
 #
-from random import shuffle,choice
+from random import shuffle,choice,randint
 
 def Impl_shuffle(x):
-  shuffle(x)
-  return x
+  y=x[:]
+  shuffle(y)
+  return y
   
 def Impl_select(x):
   if len(x)==0: return 0
@@ -15,5 +16,23 @@ def Impl_average(x):
   sum = 0
   for i in x:
     sum += i
-  return sum
+  return sum/len(x)
+  
+def Impl_remove(x):
+  if len(x)==0: return x
+  y=x[:]
+  y.pop(randint(0,len(x)-1))
+  return y
+  
+## Future Ideas
+# random subset of a list
+# something based on current time?
+
+#Tests
+p = [1,2,3,4,5,6,7,8]
+print Impl_shuffle(p)
+print p
+print Impl_select([1,2,3,4,5])
+print Impl_average([30,32,34,36])
+print Impl_remove([1,4,5,6,7,8,])
   
