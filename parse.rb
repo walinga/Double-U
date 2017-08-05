0a0725c1a1ba2971fa72cc56820f1617c1eab64f
Matthew <matthew.walinga@gmail.com>
2017-08-04 23:19:15 -0400

    Now allowing expressions as rvalues of 'let' and 'print'

--- parse.rb
+++ parse.rb
@@ -12,36 +12,60 @@ class Main
     @@vars = {} # Hash table of variables
   end
 
-  # TODO: Add type-checking
-  #       Add support for integer vars
-  #       Allow expressions as rvalues (maybe do actual parsing)
+  def checkDef(var) 
+    unless @@vars.has_key?(var)
+      raise "Double-u syntax error: Variable #{var} undeclared"
+    end
+  end
+
+  def checkFunc(func)
+    unless eval "defined? @@list.Impl_#{func}"
+      raise "Double-u syntax error: Function #{func} not defined"
+    end
+  end
+
+  # TODO: Add type-checking (if necessary)
+  #       Allow expressions as rvalues for list functions
   #       Create a formal specification of the language
   # DONE: Error checking - undefined functions and variables
+  #       Allow expressions as rvalues
+  #       Add support for integer variables
   def execline(inst)
     case inst
+      when /^ *$/  # Blank space
+      when /^;.*$/ # Comment
       when /^let +(\w+) *= *\[ *((\d *)*)\] *$/
         @@vars["#{$1}"] = "#{$2}".split.map { |x| x.to_i }
-        puts "let please" # debug
-      when /^print +(\w+).*/
+      # Expression as rvalue for 'let'
+      when /^let +(\w+) *= *(.*)$/
+        val = execline("#{$2}")
+        raise "Double-u syntax error: trying to assign void to a variable" if val.nil?
+        @@vars["#{$1}"] = val
+      when /^print +(\w+) *$/
         # Assumes array for now
-        unless @@vars.has_key?("#{$1}")
-          raise "Double-u syntax error: Variable #{$1} undeclared"
-        end
+        checkDef("#{$1}")
         print @@vars["#{$1}"], "\n"
-        puts "You printed: #{$1}" #debug
-      when /([a-z]+) +(\w+)/
-        unless eval 'defined? @@list.Impl_' + "#{$1}"
-          raise "Double-u syntax error: Function #{$1} not defined" 
+      when /^print +(.*)$/
+        val = execline("#{$1}")
+        raise "Double-u syntax error: trying to print void" if val.nil?
+        print val, "\n"
+      when /^([a-z]+) +(\w+) *$/
+        checkFunc("#{$1}")
+        checkDef("#{$2}")
+        input = @@vars["#{$2}"]
+        unless input.kind_of?(Array)
+          raise "Double-u syntax error: Invalid argument to function #{$1}"
         end
-        unless @@vars.has_key?("#{$2}")
-          raise "Double-u syntax error: Variable #{$2} undeclared"
-        end
-        res = eval '@@list.Impl_' + "#{$1}(" + @@vars["#{$2}"].to_s + ")"
-        puts "other command: #{res}"
+        return eval "@@list.Impl_#{$1}(#{input})"
+      when /([a-z]+) *\[ *((\d *)*)\] */ 
+        checkFunc("#{$1}")
+        return eval "@@list.Impl_#{$1}(#{$2.split.map { |x| x.to_i}})"
       else
         puts "oops" # debug
         raise "Double-u syntax error: Invalid instruction"
     end
+    # The default return value is nil
+    return nil
   end
 
   def run
