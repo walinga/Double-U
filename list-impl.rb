12b0e7bc7be65494d35160bd7eb5a1dae4a4a4a5
Matthew <matthew.walinga@gmail.com>
2017-08-07 16:04:08 -0400

    Fixed some gotchas with list function implementations

--- list-impl.rb
+++ list-impl.rb
@@ -14,6 +14,7 @@ def Impl_select(x)
 end
 
 def Impl_average(x)
+  return 0 if x.empty?
   sum = 0
   x.each do |i|
     sum += i
@@ -24,7 +25,7 @@ end
 def Impl_remove(x)
   return x if x.empty?
   y = Array.new(x)
-  y.delete_at(rand(x.length-1))
+  y.delete_at(rand(x.length))
   return y
 end
   
@@ -32,16 +33,5 @@ end
 # random subset of a list
 # something based on current time?
 
-#
-## Tests
-
-#  p = [1,2,3,4,5,6,7,8]
- # print Impl_shuffle(p), "\n"
-#  print p, "\n"
- # print Impl_select([1,2,3,4,5]), "\n"
-#  print Impl_average([30,32,34,35]), "\n"
- # q = [1,4,5,6,7,8]
-#  print Impl_remove(q), "\n"
- # print q, "\n"
 end
-  
+
