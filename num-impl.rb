a8fe105fa1826a85d54c4d71bf95129f9087f361
Matthew <matthew.walinga@gmail.com>
2017-09-23 23:37:41 -0400

    Start adding support for functions with integer parameters

---
+++ num-impl.rb
@@ -0,0 +1,11 @@
+#
+## Functions which take integers as input
+#
+
+class NumImpl 
+
+  def Impl_twist(i)
+    Random.new(i)
+  end
+
+end  
--- parse.rb
+++ parse.rb
@@ -1,15 +1,18 @@
 require 'set'
 require_relative 'list-impl'
+require_relative 'num-impl'
 
 class Main
   @@src
   @@list
+  @@num
   @@vars
   @@linenum
 
   def initialize(src)
     @@src = src
     @@list = ListImpl.new
+    @@num = NumImpl.new
     @@vars = {} # Hash table of variables
     @@linenum = 1 # variable; used for error messages
   end
@@ -25,14 +28,18 @@ class Main
   end
 
   def checkFunc(func)
-    unless eval "defined? @@list.Impl_#{func}"
+    unless eval "defined? @@list.Impl_#{func} || defined? @@num.Impl_#{func}"
       error "Function #{func} not defined"
     end
   end
 
   # Currently only checks if type is Array
   def checkType(var, func)
-    unless var.kind_of?(Array)
+    if var.kind_of?(Array)
+      'list'
+    elsif var.kind_of?(Integer)
+      'num'
+    else
       error "Invalid argument to function #{func}"
     end
   end
@@ -64,25 +71,30 @@ class Main
         checkDef($1)
         checkDef($2)
         a1, a2 = @@vars[$1], @@vars[$2]
-        checkType(a1, "merge")
-        checkType(a2,"merge")
+        if checkType(a1, "merge") != checkType(a2,"merge")
+          error "Type inputs to merge don't match"
+        end
         return a1 + a2
-      # List function with variable as param
+      # Num function with literal as param
+      when /^([a-z]+)! *(\d+)$/
+        checkFunc($1)
+        return eval "@@num.Impl_#{$1}(#{$2})"
+      # Any function with variable as param
       when /^([a-z]+)! +(\w+)$/
         checkFunc($1)
         checkDef($2)
         input = @@vars[$2]
-        checkType(input, $1)
-        return eval "@@list.Impl_#{$1}(#{input})"
+        type = checkType(input, $1)
+        return eval "@@#{type}.Impl_#{$1}(#{input})"
       # List function with literal as param
       when /^([a-z]+)! *\[ *((\d *)*)\]$/
         checkFunc($1)
         return eval "@@list.Impl_#{$1}(#{$2.split.map { |x| x.to_i}})"
-      # List function with result of another function as param
+      # Any function with result of another function as param
       when /^([a-z]+)! +(.*)$/
         val = execline($2)
-        checkType(val, $1)
-        return eval "@@list.Impl_#{$1}(#{val})"
+        type = checkType(val, $1)
+        return eval "@@#{type}.Impl_#{$1}(#{val})"
       else
         puts "oops" # debug
         error "Invalid instruction"
